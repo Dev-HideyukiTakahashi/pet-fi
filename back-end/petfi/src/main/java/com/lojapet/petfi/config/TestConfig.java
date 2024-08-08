2d4aaf6e7fc2bc686e3b68aa543a83777fce6726
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-08-07 23:46:15 -0300

    Organization

--- back-end/petfi/src/main/java/com/lojapet/petfi/config/TestConfig.java
+++ back-end/petfi/src/main/java/com/lojapet/petfi/config/TestConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.lojapet.petfi.entities.Client;
 import com.lojapet.petfi.entities.Dog;
@@ -15,6 +16,7 @@ import com.lojapet.petfi.repositories.DogRepository;
 import com.lojapet.petfi.repositories.UserRepository;
 
 @Configuration
+@Profile(value = "test")
 public class TestConfig implements CommandLineRunner {
 
   @Autowired
--- back-end/petfi/src/test/java/com/lojapet/petfi/PetfiApplicationTests.java
+++ back-end/petfi/src/test/java/com/lojapet/petfi/PetfiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.petfi.track;
+package com.lojapet.petfi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TrackApplicationTests {
+class PetfiApplicationTests {
 
 	@Test
 	void contextLoads() {
