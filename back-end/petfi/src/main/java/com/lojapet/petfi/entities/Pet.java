f6e9aa8a7539b8733cb3b94cfee64d4ebce0c013
unknown <dev.hideyukitakahashi@gmail.com>
2024-09-01 04:10:17 -0300

    back / photo upload config

--- back-end/petfi/src/main/java/com/lojapet/petfi/entities/Pet.java
+++ back-end/petfi/src/main/java/com/lojapet/petfi/entities/Pet.java
@@ -3,11 +3,15 @@ package com.lojapet.petfi.entities;
 import com.lojapet.petfi.entities.enums.PetType;
 import com.lojapet.petfi.entities.enums.Sex;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -15,138 +19,140 @@ import jakarta.persistence.Table;
 @Table(name = "tb_pet")
 public class Pet {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String name;
-  private Sex sex;
-  private String additionalInformation;
-  private String qrcode;
-  private String photo;
-  private Boolean wanted;
-  private PetType petType;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Sex sex;
+	private String additionalInformation;
+	private String qrcode;
 
-  @ManyToOne
-  @JoinColumn(name = "client_id")
-  private Client client;
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
+	@Column(name = "img", columnDefinition = "LONGBLOB")
+	private String photo;
+	private Boolean wanted;
+	private PetType petType;
 
-  public Pet() {
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private Client client;
 
-  }
+	public Pet() {
 
-  public Pet(Long id, String name, Sex sex, String additionalInformation, String photo, Client client,
-      PetType petType) {
-    this.id = id;
-    this.name = name;
-    this.sex = sex;
-    this.additionalInformation = additionalInformation;
-    this.photo = photo;
-    this.client = client;
-    this.petType = petType;
-    this.wanted = false;
-  }
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Pet(Long id, String name, Sex sex, String additionalInformation, String photo, Client client,
+			PetType petType) {
+		this.id = id;
+		this.name = name;
+		this.sex = sex;
+		this.additionalInformation = additionalInformation;
+		this.photo = photo;
+		this.client = client;
+		this.petType = petType;
+		this.wanted = false;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public Sex getSex() {
-    return sex;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public void setSex(Sex sex) {
-    this.sex = sex;
-  }
+	public Sex getSex() {
+		return sex;
+	}
 
-  public String getAdditionalInformation() {
-    return additionalInformation;
-  }
+	public void setSex(Sex sex) {
+		this.sex = sex;
+	}
 
-  public void setAdditionalInformation(String additionalInformation) {
-    this.additionalInformation = additionalInformation;
-  }
+	public String getAdditionalInformation() {
+		return additionalInformation;
+	}
 
-  public Pet(String qrcode) {
-    this.qrcode = qrcode;
-  }
+	public void setAdditionalInformation(String additionalInformation) {
+		this.additionalInformation = additionalInformation;
+	}
 
-  public String getQrcode() {
-    return qrcode;
-  }
+	public Pet(String qrcode) {
+		this.qrcode = qrcode;
+	}
 
-  public void setQrcode(String qrcode) {
-    this.qrcode = qrcode;
-  }
+	public String getQrcode() {
+		return qrcode;
+	}
 
-  public String getPhoto() {
-    return photo;
-  }
+	public void setQrcode(String qrcode) {
+		this.qrcode = qrcode;
+	}
 
-  public void setPhoto(String photo) {
-    this.photo = photo;
-  }
+	public String getPhoto() {
+		return photo;
+	}
 
-  public Boolean isWanted() {
-    return wanted;
-  }
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
 
-  public void setWanted(Boolean wanted) {
-    this.wanted = wanted;
-  }
+	public Boolean isWanted() {
+		return wanted;
+	}
 
-  public Client getClient() {
-    return client;
-  }
+	public void setWanted(Boolean wanted) {
+		this.wanted = wanted;
+	}
 
-  public void setClient(Client client) {
-    this.client = client;
-  }
+	public Client getClient() {
+		return client;
+	}
 
+	public void setClient(Client client) {
+		this.client = client;
+	}
 
+	public PetType getPetType() {
+		return petType;
+	}
 
-  public PetType getPetType() {
-    return petType;
-  }
+	public void setPetType(PetType petType) {
+		this.petType = petType;
+	}
 
-  public void setPetType(PetType petType) {
-    this.petType = petType;
-  }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Pet other = (Pet) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    return true;
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pet other = (Pet) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
