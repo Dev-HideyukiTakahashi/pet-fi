d4a595553add636511412675538107787ac224ac
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-09-06 05:51:48 -0300

    refactor qrcode

--- front-end/src/app/components/pets/qrcode/qrcode.component.ts
+++ front-end/src/app/components/pets/qrcode/qrcode.component.ts
@@ -6,8 +6,8 @@ import { Pets } from '../../../models/pets';
 import { PetService } from '../../../services/pet.service';
 import { FormsModule } from '@angular/forms';
 import { QRCodeModule } from 'angularx-qrcode';
-import html2canvas from 'html2canvas';
 import { SafeValue } from '@angular/platform-browser';
+import { Client } from '../../../models/client';
 
 @Component({
   selector: 'app-qrcode',
@@ -18,42 +18,47 @@ import { SafeValue } from '@angular/platform-browser';
 })
 export class QrcodeComponent {
 
-  idPet!: number;
+  searchOption = "Código";
+  value!: string;
+
   pet!: Pets;
   petService = inject(PetService);
+
 
   qrcodeView: boolean = false;
   qrdata!: string;
   qrCodeDownloadLink: SafeValue = '';
 
-
+  constructor() {
+  }
 
   onChange(url: SafeValue) {
     this.qrCodeDownloadLink = url;
   }
 
-  findPetById(id: number) {
-    if (id == null) {
+  findById(value: string) {
+    if (value == null || value == "") {
       Swal.fire({
         icon: "error",
         title: "Oops...",
-        text: "Preencha o campo para realizar a busca!",
+        text: "Preencha o código para realizar a busca!",
       });
     } else {
-      this.petService.findById(id).subscribe({
+      this.petService.findById(Number.parseInt(value)).subscribe({
         next: response => {
-          this.pet = response,
-            this.qrcodeView = true,
-            this.qrdata = this.pet.qrcode;
+          this.pet = response
+          this.qrcodeView = true
+          this.qrdata = this.pet.qrcode
+          this.value = ""
         },
         error: e => {
           if (this.pet != null) {
-            this.pet.client.name = "",
-              this.pet.name = "",
-              this.pet.id = 0,
-              this.idPet = Number(undefined);
+            this.pet.client.name = ""
+            this.pet.name = ""
+            this.pet.id = 0
+            this.value = ""
           } else {
-            this.idPet = Number(undefined);
+            this.value = "";
           }
           this.qrcodeView = false;
           Swal.fire("Código não localizado!")
@@ -63,3 +68,4 @@ export class QrcodeComponent {
   }
 
 }
+
